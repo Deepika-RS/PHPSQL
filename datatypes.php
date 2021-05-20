084046420494d5d1e1831195f41900cc67a5d23f
Deepika-RS <84494892+Deepika-RS@users.noreply.github.com>
2021-05-20 12:15:03 +0530

    first commit

---
+++ datatypes.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+// STRING data type
+$x = "Hello world!";
+$y = 'Hello world!';
+echo $x;
+echo "<br />";
+echo $y;
+echo "<hr />";
+
+// INTEGER
+
+$num1=10;
+ echo var_dump($num1);
+echo "<hr />";
+
+//floatval
+
+$num2=21.345;
+var_dump($num2);
+echo "<hr />";
+
+// ARRAY
+$sport=array("a","b","c","d");
+var_dump($sport);
+echo "<hr />";
+
+//Null
+$num3=null;
+var_dump($num3);
+$x=true;
+$y=false;
+var_dump($x);
+var_dump($y);
+?>
+</body>
+</html>
