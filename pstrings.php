09f6e1e6d3c9293ca6a7bbed6b48794cfb53aa44
Deepika-RS <rv.deepika009@gmail.com>
2021-05-20 23:32:40 +0530

    first commit

---
+++ pstrings.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+// word count
+echo str_word_count("Hello world");
+echo "<hr />";
+
+//reverse words
+echo strrev("hello world!");
+echo "<hr />";
+
+//search for the string inside a string
+echo strpos("Hello world!","world");
+echo "<hr />";
+
+//replace text inside a string
+echo str_replace("world","Jhon","Hello world!");
+echo "<hr />";
+?>
+</body>
+</html>
+
