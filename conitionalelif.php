2243151bd430ff1af1de5cceb922057390737b12
Deepika-RS <rv.deepika009@gmail.com>
2021-05-24 17:31:13 +0530

    first commit

---
+++ conitionalelif.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$x=78;
+if($x<50){
+echo"F";
+}
+	else if ($x>=50 && $x<60){
+	echo"D";}
+ else if ($x>=60 && $x<70){
+	 echo"C";
+ }
+ else if ($x>=70 && $x<80){
+	 echo"B";
+ }
+ else if ($x>=80 && $x<90){
+	 echo"A";
+ }
+ else if ($x>=90){
+	 echo "A+";
+ }
+ ?>
+ </body>
+ </html>
+ 
+ 
+ 
+ 
+ 
