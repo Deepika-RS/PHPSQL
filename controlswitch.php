6c728e8ed15077b60e281461d9f396c99ee600c8
Deepika-RS <rv.deepika009@gmail.com>
2021-05-24 17:33:36 +0530

    first commit

---
+++ controlswitch.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$car="benze";
+switch($car){
+	case "volvo":
+	echo"you drive a volvo car";
+	break;
+	case "Hyundai":
+	echo"you drive a hyundai car";
+	break;
+	case "benze":
+	echo"you drive a benze car";
+	break;
+	default:echo"you dont drive ";
+}
+?>
+</body>
+</html>
