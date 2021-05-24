8a9d4daf89ac1570bee6cb9a41b25b72606fce92
Deepika-RS <rv.deepika009@gmail.com>
2021-05-24 17:51:47 +0530

    first commit

---
+++ functions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+   //basic function_exists
+   function display(){
+	   echo"My first function in php";
+   }
+   display();
+   echo"<hr />";
+   
+   //function with arguments
+   function myCar($car){
+	   echo"name of my car is $car <br />";
+   }
+   myCar("volvo");
+   myCar("benze");
+   ?>
+   </html>
+   </body>
+   
