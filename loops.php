8e72a55d9e54222fd8159253ff5a5681e4dbb822
Deepika-RS <rv.deepika009@gmail.com>
2021-05-24 17:35:08 +0530

    first commit

---
+++ loops.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+//while
+$x=1;
+while ($x<10 ){
+	echo  "the number is $x <br />";
+	$x++;
+}
+echo"<hr />";
+
+//do while
+$y=10;
+do {
+	echo  "the number is $y <br />";
+	$y++;
+}while ($y<10);
+?>
+</body>
+</html>
+
