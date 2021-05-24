7200cbfe09471382ff991bf44e9eb0738caa9ae9
Deepika-RS <rv.deepika009@gmail.com>
2021-05-24 17:45:03 +0530

    first commit

---
+++ foreachfor.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+  //for loop
+  for($x=0;$x<=5;$x++){
+	  echo "$x <br />";
+  }
+  echo"<hr ?>";
+  // foreach loop
+  $xy = array ("volvo" , "bmw" , "ford" , "tata");
+  foreach($xy as $val){
+	  echo "$val<br/>";
+  }
+  ?>
+  <body>
+  </html>
